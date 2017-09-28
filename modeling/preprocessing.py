71df9f51b70dad7e92d9a3c18ea5d610e3d74f76
tqhdesilva <tdesilvaqihao@gmail.com>
2017-09-28 10:39:24 -0700

    added modeling for 7.06f, high skill modeling, tweaked pipeline

--- modeling/preprocessing.py
+++ modeling/preprocessing.py
@@ -1,39 +1,76 @@
+import os
+import sys
 import pandas as pd
+import numpy as np
 
-rad_cats = ['radiant_' + str(x) for x in xrange(0, 115)]
-dire_cats = ['dire_' + str(x) for x in xrange(0, 115)]
+sys.path.append(os.path.join( '..', 'data'))
+from build_db import connect
+from db_helpers import parse_date
 
-def expand_players(df):
-    df = df.copy()
+def parse_pb(pb_list):
+    # return t1_picks, t2_picks, t1_bans, t2_bans
+    pb_df = pd.DataFrame(data=map(lambda x: x.values(), pb_list), columns=pb_list[0].keys())
+    picks = pb_df[pb_df['is_pick']]
+    bans = pb_df[~pb_df['is_pick']]
+    team1 = picks['team'].iloc[0]
+    team2 = picks['team'].iloc[1]
+    t1_picks, t2_picks = tuple(picks[picks['team']==team1]['hero_id']),\
+                         tuple(picks[picks['team']==team2]['hero_id'])
 
-def parse_players(players_list):
-    hero_id = [player['hero_id'] for player in players_list]
-    radiant = [player['player_slot'] < 128 for player in players_list]
-    df = pd.DataFrame({'hero_id' : hero_id, 'radiant': radiant})
-    radiant_heroes = df[df['radiant']]['hero_id']
-    dire_heroes = df[~df['radiant']]['hero_id']
+    t1_bans = []
+    t2_bans = []
+
+    ban_schema = [team1, team2, team1, team2, team2, team1, team2, team1, team1, team2]
+    ban_counter = 0
+    for banning_team in ban_schema:
+        try:
+            next_ban = bans.iloc[ban_counter]
+            if next_ban['team'] == banning_team:
+                banned_hero = next_ban['hero_id']
+                ban_counter += 1
+            else:
+                banned_hero = None
+        except IndexError:
+            banned_hero = None
+        if banning_team == team1:
+            t1_bans.append(banned_hero)
+        else:
+            t2_bans.append(banned_hero)
+    t1_bans = tuple(t1_bans)
+    t2_bans = tuple(t2_bans)
+    return {'t1_picks': t1_picks, 't2_picks': t2_picks, 't1_bans': t1_bans,
+            't2_bans': t2_bans, 'team1': team1, 'team2': team2}
+
+def parse_players(player_list):
+    radiant_heroes = [player['hero_id'] for player in player_list if player['player_slot'] < 128]
+    dire_heroes = [player['hero_id'] for player in player_list if player['player_slot'] >= 128]
     return radiant_heroes, dire_heroes
 
-def make_dummy(radiant_heroes, dire_heroes):
-    # columns = dict()
-    # for i in xrange(0, 115): # wtf is hero_id = 0?
-        # columns[ 'radiant_' + str(i) ] = [0]
-        # columns[ 'dire_' + str(i) ] = [0]
-    # for hero in radiant_heroes:
-        # columns['radiant_' + str(hero)][0] = 1
-    # for hero in dire_heroes:
-        # columns['dire_' + str(hero)][0] = 1
-    # df = pd.DataFrame(columns)
-    # return df
-    rad_dummies = pd.get_dummies(radiant_heroes).reindex(columns=rad_cats,
-                                           fill_value=0).sum()
-    dire_dummies = pd.get_dummies(dire_heroes).reindex(columns=dire_cats,
-                                           fill_value=0).sum()
-    return pd.concat([rad_dummies, dire_dummies], axis=1)
+def preprocess_players(df):
+    df = df.copy()
+    parsed_series = df['players'].map(parse_players)
+    radiant_picks = parsed_series.map(lambda x: x[0])
+    dire_picks = parsed_series.map(lambda x: x[1])
+    picks_df = pd.DataFrame(data={'radiant_picks': radiant_picks,
+                                  'dire_picks': dire_picks})
+    radiant_dummies = pd.get_dummies(picks_df['radiant_picks'].apply(pd.Series).stack(),
+                                     prefix='radiant', prefix_sep='_').sum(level=0)
+    dire_dummies = pd.get_dummies(picks_df['dire_picks'].apply(pd.Series).stack(),
+                                  prefix='dire', prefix_sep='_').sum(level=0)
+    df = pd.concat([df[['match_id', 'radiant_win']], radiant_dummies, dire_dummies],
+                   axis=1)
+    return df
 
-def make_dummies(df):
-    heroes = df['players'].map(parse_players)
-    dummies_dfs = map(lambda x: make_dummy(*x), heroes)
-
-    dummies_df = pd.concat(dummies_dfs, axis = 0)
-    return dummies_df
+def preprocess(df):
+    df = df.copy()
+    parsed_series = df['picks_bans'].map(parse_pb)
+    t1_picks = parsed_series.map(lambda x: x['t1_picks'])
+    t2_picks = parsed_series.map(lambda x: x['t2_picks'])
+    team1 = parsed_series.map(lambda x: x['team1'])
+    picks_df = pd.DataFrame(data={'t1_picks': t1_picks, 't2_picks':t2_picks, 'team1': team1})
+    t1_dummies = pd.get_dummies(picks_df['t1_picks'].apply(pd.Series).stack(), prefix='t1', prefix_sep='_').sum(level=0)
+    t2_dummies = pd.get_dummies(picks_df['t2_picks'].apply(pd.Series).stack(), prefix='t2', prefix_sep='_').sum(level=0)
+    df['team1_win'] = (picks_df['team1'] == 1) ^ (df['radiant_win'])
+    df = pd.concat([df[['match_id', 'team1_win']], t1_dummies, t2_dummies], axis=1)
+    df['team1'] = team1
+    return df
